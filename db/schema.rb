1657871c2abdb2c458ac57b13058234531b03a7a
Theresa <tmannschatz@gmail.com>
2020-09-17 16:35:01 +0200

    Create Students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_134023) do
+ActiveRecord::Schema.define(version: 2020_09_17_142112) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "short_name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
