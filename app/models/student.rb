cc349c5536b465425746cd952f11bf8b92a02b36
Theresa <tmannschatz@gmail.com>
2020-09-18 22:32:23 +0200

    Add secure password to students; add password fields to sign up and edit form

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,6 @@ class Student < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 150 }, uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
