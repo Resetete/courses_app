994638982eb4503fb0d3eac0938e8a24f066ce39
Theresa <tmannschatz@gmail.com>
2020-09-24 15:05:03 +0200

    Add courses resource and show page, add links to courses

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,5 +8,5 @@ class Student < ApplicationRecord
   has_secure_password
 
   has_many :enrollments
-  has_many :courses, through: enrollments
+  has_many :courses, through: :enrollments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'courses#index'
 
   get 'about', to: 'pages#about'
-  get 'courses/new', to: 'courses#new'
   resources :students, except: [:destroy]
+  resources :courses
   get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
-  delete 'logout', to: 'logins#destroy' 
+  delete 'logout', to: 'logins#destroy'
 end
